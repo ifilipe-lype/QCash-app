5a35c42968e947c71ad7a0ff2cb0a9eddef2f0a1
ifilipe-lype <filipe7lype@gmail.com>
2021-03-19 15:42:03 +0100

    add state for UI

--- src/screens/Incomes/index.js
+++ src/screens/Incomes/index.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { View, Text, TouchableOpacity, TouchableHighlightBase } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { Colors } from "../../constants";
+
 
 import { Entypo, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import styles from "./styles";
 
 export default function IncomeScreen() {
+
+    const [showDoneEntries, setShowDoneEntries] = useState(true);
+    const [showNotDoneEntries, setNotShowDoneEntries] = useState(true);
+
+    function toogleVisibility(doneEntries){
+        if(doneEntries){
+            setShowDoneEntries(!showDoneEntries);
+        } else {
+            setNotShowDoneEntries(!showNotDoneEntries);
+        }
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.screenLabel}>Ganhos de Março, 2021</Text>
-
 
                 <View style={styles.headerRightActions}>
                     <TouchableOpacity style={styles.headerAction}>
@@ -23,34 +37,39 @@ export default function IncomeScreen() {
                 </View>
             </View>
             <View style={styles.incomes}>
-                <View style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        paddingBottom: 12,
-                        borderBottomWidth: 1,
-                        borderColor: "rgba(0,0,0, .05)"
-                    }}>
-                    <TouchableOpacity style={styles.incomeResult}>
-                        <MaterialCommunityIcons name="calendar-check-outline" size={32} color="rgba(0,0,0, .65)" />
+                <View style={styles.incomeResults}>
+                    <TouchableOpacity onPress={() => toogleVisibility(true)} style={styles.incomeResult}>
+                        {
+                            showDoneEntries ? (
+                                <MaterialCommunityIcons name="calendar-check-outline" size={28} color="rgba(0,0,0, .65)" />
+                            ) : (
+                                <MaterialCommunityIcons name="eye-off-outline" size={28} color="rgba(0,0,0, .35)" />
+                            )
+                        }
                         <View style={{
                             marginLeft: 6
                         }}>
                             <Text style={styles.resultLabel}>Efetuados</Text>
                             <Text style={{
                                 fontSize: 15,
-                                color: "green"
+                                color: `rgb(${Colors.greenRGB})`
                             }}>645,000 kz</Text>
                         </View>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.incomeResult}>
-                        <MaterialCommunityIcons name="calendar-blank-outline" size={32} color="rgba(0,0,0, .65)" />
+                    <TouchableOpacity onPress={() => toogleVisibility(false)} style={styles.incomeResult}>
+                        {
+                            showNotDoneEntries ? (
+                                <MaterialCommunityIcons name="calendar-blank-outline" size={28} color="rgba(0,0,0, .65)" />
+                            ) : (
+                                <MaterialCommunityIcons name="eye-off-outline" size={28} color="rgba(0,0,0, .35)" />
+                            )
+                        }
                         <View style={{
                             marginLeft: 6
                         }}>
                             <Text style={styles.resultLabel}>Por efetuar</Text>
-                            <Text>145,000 kz</Text>
+                            <Text style={styles.resultAmount}>145,000 kz</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
