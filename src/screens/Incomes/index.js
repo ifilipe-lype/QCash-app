5c7d945bdf5ef61c04f8abb7f1e3e90ba2cd8788
ifilipe-lype <filipe7lype@gmail.com>
2021-03-21 22:18:06 +0100

    changes fakeData content

--- src/screens/Incomes/index.js
+++ src/screens/Incomes/index.js
@@ -13,88 +13,88 @@ import styles from "./styles";
 
 const fakeData = [
     {
-        description: "ganho #1",
-        amount: "1250",
+        description: "ganho #1 lorem sf lidf slidf sidfsfe sfsdf lifsdf fsfi dfsfi sfie dsfsd",
+        amount: "1,250,000,000",
         done: true,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #2",
         amount: "3000",
         done: true,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #3",
         amount: "4000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #4",
         amount: "1000",
         done: true,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #5",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #6",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #7",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #8",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #10",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #11",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #12",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
     {
         description: "ganho #13",
         amount: "1000",
         done: false,
         done_at: "01/12/2021",
-        created_at: "12/12/2020"
+        created_at: Date.now()
     },
 ]
 
