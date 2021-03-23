965c7fa8c7c6a3f7905e28dc67481d0cf9f89de6
ifilipe-lype <filipe7lype@gmail.com>
2021-03-23 01:30:31 +0100

    exports a entry details viewer component

--- src/components/EntryDetailsViewer/styles.js
+++ src/components/EntryDetailsViewer/styles.js
@@ -6,18 +6,73 @@ export default StyleSheet.create({
         borderRadius: 6,
         overflow: "hidden"
     },
-    header:{
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
         paddingVertical: 24,
-        paddingHorizontal: 12
+        paddingHorizontal: 12,
+        borderBottomWidth: 1,
+        borderColor: "rgba(0,0,0, .08)"
+    },
+    infoBox: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginRight: 16
+    },
+    infoLabel: {
+        fontWeight: "700",
+        fontSize: 14,
+        color: `rgba(0,0,0, .55)`,
+        marginLeft: 6
+    },
+    detail: {
+        marginVertical: 6,
+    },
+    detailIcon: {
+        marginRight: 6,
+        color: `rgba(0,0,0, .45)`
+    },
+    detailLabel:{
+        fontSize: 14,
+        color: `rgba(0,0,0, .45)`,
+    },
+    detailValue:{
+        marginLeft: 26,
+        fontSize: 14,
+        color: `rgba(0,0,0, .85)`,
     },
     main: {
-        paddingVertical: 24,
-        paddingHorizontal: 12
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        overflow: "hidden"
     },
     footer: {
-        justifyContent: "center",
+        paddingVertical: 24,
+        paddingHorizontal: 24
+
+    },
+    editBtn: (bgColor) => ({
         alignItems: "center",
-        paddingBottom: 24,
-        
-    }
+        backgroundColor: `rgb(${bgColor})`,
+        padding: 8,
+        marginBottom: 12,
+        borderRadius: 12,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 1.84,
+
+        elevation: 2,
+    }),
+    makeDoneBtn: (bgColor) => ({
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: `rgb(${bgColor})`,
+        padding: 8,
+        marginBottom: 12,
+        borderRadius: 12,
+    })
 });
