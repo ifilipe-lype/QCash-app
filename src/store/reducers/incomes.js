e054143e46a1fbc92e8c5a930692a661c8aa1bff
ifilipe-lype <filipe7lype@gmail.com>
2021-03-25 22:44:06 +0100

    exports a reducer function

--- src/store/reducers/incomes.js
+++ src/store/reducers/incomes.js
@@ -1,3 +1,6 @@
+import 'react-native-get-random-values';
+import { v4 as uuid } from 'uuid';
+
 import { createSlice } from "@reduxjs/toolkit";
 
 import initialValue from "./initial-states";
@@ -5,7 +8,14 @@ import initialValue from "./initial-states";
 const incomesReducer = createSlice({
     name: "incomes",
     initialState: [...initialValue.incomes],
-    reducers: {}
+    reducers: {
+        addIncome: (state, { playload }) => {
+            const newIncome = { ...playload, id: uuid()}
+            state.push(newIncome);
+        }
+    }
 });
+
+export const { addIncome } = incomesReducer.actions;
 
 export default incomesReducer.reducer;
