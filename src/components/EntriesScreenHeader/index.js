58924983c2e55bd53785ba7d44bc420ef35a6c5b
ifilipe-lype <filipe7lype@gmail.com>
2021-03-26 23:49:20 +0100

    exports a header component for both income and outcome screens

---
+++ src/components/EntriesScreenHeader/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import { Entypo, Feather } from '@expo/vector-icons';
+
+
+import { Colors } from "../../constants";
+
+
+import styles from "./styles";
+
+export default function EntriesScreenHeader({ isIncome ,title, setShowAddEntryForm }) {
+
+    return (
+        <View style={styles.header}>
+            <Text style={styles.screenLabel}>{title}</Text>
+
+            <View style={styles.headerRightActions}>
+                <TouchableOpacity style={styles.headerAction}>
+                    <Feather name="search" size={20} color="rgba(0,0,0, .35)" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.headerAction, { paddingRight: 0 }]}
+                    onPress={() => setShowAddEntryForm(true)}
+                >
+                    <Entypo name="add-to-list" size={24} color={`rgb(${isIncome ? Colors.greenRGB : Colors.redRGB})`} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
---
+++ src/components/EntriesScreenHeader/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../../constants";
+
+export default StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: "white",
+        paddingHorizontal: 12,
+        height: 48,
+        paddingVertical: 8
+    },
+    screenLabel: {
+        fontSize: 14,
+        color: `rgba(${Colors.purpleDarkRGB}, .5)`
+    },
+    headerRightActions: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    headerAction: {
+        paddingHorizontal: 12,
+        paddingVertical: 8,
+    }
+});
