46c6d224aab6ceed8bd89078efce6a2dad5f359c
ifilipe-lype <filipe7lype@gmail.com>
2021-03-27 14:25:37 +0100

    adds persisting store provider

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from "react-redux";
 
 import RootNavigator from "./src/navigation";
-import store from "./src/store";
+import store, { persistor } from "./src/store";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <RootNavigator />
-        <StatusBar style="auto" />
+        <PersistGate loading={null} persistor={persistor} >
+          <RootNavigator />
+          <StatusBar style="auto" />
+        </PersistGate>
       </Provider>
     </SafeAreaProvider>
   )
