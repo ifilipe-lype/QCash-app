27528a64342d52231e2ae8573bcb702efe05ca21
ifilipe-lype <filipe7lype@gmail.com>
2021-04-02 13:39:05 +0100

    exports a function formattin numbers as money

--- src/utils/index.js
+++ src/utils/index.js
@@ -45,3 +45,7 @@ export function getSeparatedIncomes(list){
 
     return [doneEntries, notDoneEntries];
 }
+
+export function formatMoney(amount){
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
