785593fcd39ddf0c7381626dd836e40dd8ea7a40
ifilipe-lype <filipe7lype@gmail.com>
2021-04-07 00:24:13 +0100

    removes console.log call

--- src/store/reducers/sheets.js
+++ src/store/reducers/sheets.js
@@ -2,7 +2,6 @@ import 'react-native-get-random-values';
 import { createSlice } from "@reduxjs/toolkit";
 
 import { generateId } from "../../utils";
-import initStt from "./initial-states";
 
 const d = new Date();
 const currentDate = `${d.getMonth()}-${d.getFullYear()}`;
@@ -16,8 +15,8 @@ const sheetsReducer = createSlice({
                 id: generateId(),
                 month: d.getMonth(),
                 year: d.getFullYear(),
-                incomes: initStt.incomes,
-                outcomes: initStt.outcomes
+                incomes: [],
+                outcomes: []
             }
         }
     },
@@ -26,7 +25,6 @@ const sheetsReducer = createSlice({
             const { month, year } = payload;
             state.sheets[`${month}-${year}`] = { id: generateId(), ...payload };
             state.active = payload;
-            console.log("States: ", state.sheets);
         },
         setActiveSheetByDate: (state, { payload }) => {
             const { month, year } = payload;
